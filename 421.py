facd0334f2b7bc7b2d0fa1b8eb74a577a6971985
bricso1 <bricsoDev@gmail.com>
2024-10-17 09:55:19 +0200

    Create 421.py
    just def functions for random

---
+++ 421.py
@@ -0,0 +1,21 @@
+# pour i dans 3
+#   dé1=dé1()
+#   dé2=dé2()
+#   dé3=dé3()
+#   afficher dé1,dé2,dé3
+#   total=dé1+dé2+dé3
+#   si total=7 
+#     si dé1=4 ou dé2=4 ou dé3=4:
+#       afficher "vous avez gagné!"
+#     sinon:
+#       afficher "réesseyer"
+#   sinon:
+#    afficher "réesseyer" 
+from random import *
+  dé2=randint(1,6)
+def dé1():
+  dé1=randint(1,6)
+def dé2()
+  dé2=randint(1,6)
+def dé3():
+  dé3=randint(1,6)
