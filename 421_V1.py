78fbcb5e1b82a09a0ecc77b7fdec789c3fd251c4
bricso1 <bricsoDev@gmail.com>
2024-10-22 14:55:40 +0200

    Update and rename 421.py to 421_V1.py and Finish the code

---
+++ 421_V1.py
@@ -0,0 +1,18 @@
+from random import *
+  dé2=randint(1,6)
+def dé1():
+  dé1=randint(1,6)
+def dé2()
+  dé2=randint(1,6)
+def dé3():
+  dé3=randint(1,6)
+print(dé1(),dé2(),dé3())
+total=dé1+dé2+dé3
+if total==7
+  if dé1()==4 or dé2()==4 or dé3()==4 
+    print("vous avez gangné !")
+  else:
+    print("réesseyer ")
+  else:
+     print("réesseyer")
+
