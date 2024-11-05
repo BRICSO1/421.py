771cbdb84df3e4b25fa95adb7547aaee32917c78
bricso1 <bricsoDev@gmail.com>
2024-11-05 10:43:05 +0100

    Update 421_V1.py
    delecting additional and false code for #6 pr

--- 421_V1.py
+++ 421_V1.py
@@ -14,19 +14,3 @@ if total==7
     print("réesseyer ")
   else:
      print("réesseyer")
-from random import *
-def dé1():
-  dé1=randint(1,6)
-def dé2():
-  dé2=randint(1,6)
-def dé3():
-  dé3=randint(1,6)
-print(dé1(),dé2(),dé3())
-total=dé1+dé2+dé3
-if total==3
- if dé1()==1 or dé2()==1 or dé3()==1 
-    print("vous avez gangné !")
-  else:
-    print("réesseyer ")
-  else:
-     print("réesseyer")
